1b94ac369bce07cc10a089c21987cece89f00d1d
narak <narak@DESKTOP-H5QC2T0>
2025-09-08 16:50:18 +0900

    計算の追加

--- trainingjava/src/com/example/App.java
+++ trainingjava/src/com/example/App.java
@@ -2,6 +2,10 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
+    	int a = 10;
+       	int b = 10;
+       	System.out.println(++a + 50);
+       	System.out.println(b++ + 50);
         System.out.println("Hello, Guys");
     }
 }
